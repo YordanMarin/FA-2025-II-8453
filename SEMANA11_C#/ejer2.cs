5891b75658e14672d877b730aad1ed79516aaecd
yordan <yordanmarinsovero@gmail.com>
2025-10-28 12:23:03 -0500

    sem10

---
+++ SEMANA11_C#/ejer2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA11_C_
+{
+    internal class ejer2
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { -2, 5, -1, 0, 8, -6, 3 };
+
+            Console.Write("Números: ");
+
+            foreach(int i in num) 
+                Console.Write(i+" ");
+
+            Console.Write("\nNumeros reemplazados: ");
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i]<0)
+                    num[i] = 0;
+                Console.Write(num[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SEMANA11_C#/ejer7.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMANA11_C_
+{
+    internal class ejer7
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de números: ");
+            int cant = int.Parse(Console.ReadLine());
+
+            int[] num = new int[cant];
+            Random rnd = new Random();
+
+            Console.Write("\nNúmeros generados: ");
+            for(int i = 0; i < num.Length; i++)
+            {
+                num[i] = rnd.Next(1, 51);
+                Console.Write(num[i] +" ");
+            }
+
+            Console.Write("\n\nIngrese el número a buscar: ");
+            int buscar = int.Parse(Console.ReadLine());
+
+            for(int i = 0;i < cant; i++)
+            {
+                if(num[i] == buscar)
+                {
+                    Console.WriteLine("Número existe");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Número no existe");
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
